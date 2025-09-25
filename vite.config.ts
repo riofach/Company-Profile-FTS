809794deb70aeb8d8b5e3cd40ab4753155e02db9
riofach <fachrio.raditya@gmail.com>
2025-09-25 13:14:32 +0700

    feat: allow railway host in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,16 @@ import { componentTagger } from 'lovable-tagger';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-	server: {
-		host: '::',
-		port: 8080,
-	},
 	plugins: [react(), mode === 'development' && componentTagger()].filter(Boolean),
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	server: {
+		preview: {
+			host: true,
+			port: 8080,
 		},
 	},
 	build: {
